a70ebdb2194106a87773c67ce8b7f7392651b966
Veltys <veltys@veltys.es>
2021-02-17 17:53:08 +0100

    FIX: Various errors and warnings
    self issues, indent, unused vars and avoid using reserved words

--- ABC.py
+++ ABC.py
@@ -7,157 +7,157 @@ import progressbar
 
 class ABC:
 
-    def __init__(_self, conf):
-        _self.conf = conf
-        _self.foods = np.zeros((_self.conf.FOOD_NUMBER, _self.conf.DIMENSION))
-        _self.f = np.ones((_self.conf.FOOD_NUMBER))
-        _self.fitness = np.ones((_self.conf.FOOD_NUMBER)) * np.iinfo(int).max
-        _self.trial = np.zeros((_self.conf.FOOD_NUMBER))
-        _self.prob = [0 for x in range(_self.conf.FOOD_NUMBER)]
-        _self.solution = np.zeros((_self.conf.DIMENSION))
-        _self.globalParams = [0 for x in range(_self.conf.DIMENSION)]
-        _self.globalTime = 0
-        _self.evalCount = 0
-        _self.cycle = 0
-        _self.experimentID = 0
-        _self.globalOpts = list()
+    def __init__(self, conf):
+        self.conf = conf
+        self.foods = np.zeros((self.conf.FOOD_NUMBER, self.conf.DIMENSION))
+        self.f = np.ones((self.conf.FOOD_NUMBER))
+        self.fitness = np.ones((self.conf.FOOD_NUMBER)) * np.iinfo(int).max
+        self.trial = np.zeros((self.conf.FOOD_NUMBER))
+        self.prob = [0 for _ in range(self.conf.FOOD_NUMBER)]
+        self.solution = np.zeros((self.conf.DIMENSION))
+        self.globalParams = [0 for _ in range(self.conf.DIMENSION)]
+        self.globalTime = 0
+        self.evalCount = 0
+        self.cycle = 0
+        self.experimentID = 0
+        self.globalOpts = list()
 
-        if (_self.conf.SHOW_PROGRESS):
-            _self.progressbar = progressbar.ProgressBar(max_value=_self.conf.MAXIMUM_EVALUATION)
+        if (self.conf.SHOW_PROGRESS):
+            self.progressbar = progressbar.ProgressBar(max_value=self.conf.MAXIMUM_EVALUATION)
         if (not(conf.RANDOM_SEED)):
             random.seed(conf.SEED)
 
-    def calculate_function(_self, sol):
+    def calculate_function(self, sol):
         try:
-            if (_self.conf.SHOW_PROGRESS):
-                _self.progressbar.update(_self.evalCount)
-            return _self.conf.OBJECTIVE_FUNCTION(sol)
+            if (self.conf.SHOW_PROGRESS):
+                self.progressbar.update(self.evalCount)
+            return self.conf.OBJECTIVE_FUNCTION(sol)
 
         except ValueError as err:
             print(
                 "An exception occured: Upper and Lower Bounds might be wrong. (" + str(err) + " in calculate_function)")
             sys.exit()
 
-    def calculate_fitness(_self, fun):
-        _self.increase_eval()
+    def calculate_fitness(self, fun):
+        self.increase_eval()
         if fun >= 0:
             result = 1 / (fun + 1)
         else:
             result = 1 + abs(fun)
         return result
 
-    def increase_eval(_self):
-        _self.evalCount += 1
+    def increase_eval(self):
+        self.evalCount += 1
 
-    def stopping_condition(_self):
-        status = bool(_self.evalCount >= _self.conf.MAXIMUM_EVALUATION)
-        if(_self.conf.SHOW_PROGRESS):
-          if(status == True and not( _self.progressbar._finished )):
-               _self.progressbar.finish()
+    def stopping_condition(self):
+        status = bool(self.evalCount >= self.conf.MAXIMUM_EVALUATION)
+        if(self.conf.SHOW_PROGRESS):
+            if(status == True and not( self.progressbar._finished )):
+                self.progressbar.finish()
         return status
 
-    def memorize_best_source(_self):
-        for i in range(_self.conf.FOOD_NUMBER):
-            if (_self.f[i] < _self.globalOpt and _self.conf.MINIMIZE == True) or (_self.f[i] >= _self.globalOpt and _self.conf.MINIMIZE == False):
-                _self.globalOpt = np.copy(_self.f[i])
-                _self.globalParams = np.copy(_self.foods[i][:])
+    def memorize_best_source(self):
+        for i in range(self.conf.FOOD_NUMBER):
+            if (self.f[i] < self.globalOpt and self.conf.MINIMIZE == True) or (self.f[i] >= self.globalOpt and self.conf.MINIMIZE == False):
+                self.globalOpt = np.copy(self.f[i])
+                self.globalParams = np.copy(self.foods[i][:])
 
-    def init(_self, index):
-        if (not (_self.stopping_condition())):
-            for i in range(_self.conf.DIMENSION):
-                _self.foods[index][i] = random.random() * (_self.conf.UPPER_BOUND - _self.conf.LOWER_BOUND) + _self.conf.LOWER_BOUND
-            _self.solution = np.copy(_self.foods[index][:])
-            _self.f[index] = _self.calculate_function(_self.solution)[0]
-            _self.fitness[index] = _self.calculate_fitness(_self.f[index])
-            _self.trial[index] = 0
+    def init(self, index):
+        if (not (self.stopping_condition())):
+            for i in range(self.conf.DIMENSION):
+                self.foods[index][i] = random.random() * (self.conf.UPPER_BOUND - self.conf.LOWER_BOUND) + self.conf.LOWER_BOUND
+            self.solution = np.copy(self.foods[index][:])
+            self.f[index] = self.calculate_function(self.solution)[0]
+            self.fitness[index] = self.calculate_fitness(self.f[index])
+            self.trial[index] = 0
 
-    def initial(_self):
-        for i in range(_self.conf.FOOD_NUMBER):
-            _self.init(i)
-        _self.globalOpt = np.copy(_self.f[0])
-        _self.globalParams = np.copy(_self.foods[0][:])
+    def initial(self):
+        for i in range(self.conf.FOOD_NUMBER):
+            self.init(i)
+        self.globalOpt = np.copy(self.f[0])
+        self.globalParams = np.copy(self.foods[0][:])
 
-    def send_employed_bees(_self):
+    def send_employed_bees(self):
         i = 0
-        while (i < _self.conf.FOOD_NUMBER) and (not (_self.stopping_condition())):
+        while (i < self.conf.FOOD_NUMBER) and (not (self.stopping_condition())):
             r = random.random()
-            _self.param2change = (int)(r * _self.conf.DIMENSION)
+            self.param2change = (int)(r * self.conf.DIMENSION)
 
             r = random.random()
-            _self.neighbour = (int)(r * _self.conf.FOOD_NUMBER)
-            while _self.neighbour == i:
+            self.neighbour = (int)(r * self.conf.FOOD_NUMBER)
+            while self.neighbour == i:
                 r = random.random()
-                _self.neighbour = (int)(r * _self.conf.FOOD_NUMBER)
-            _self.solution = np.copy(_self.foods[i][:])
+                self.neighbour = (int)(r * self.conf.FOOD_NUMBER)
+            self.solution = np.copy(self.foods[i][:])
 
             r = random.random()
-            _self.solution[_self.param2change] = _self.foods[i][_self.param2change] + (
-                        _self.foods[i][_self.param2change] - _self.foods[_self.neighbour][_self.param2change]) * (
+            self.solution[self.param2change] = self.foods[i][self.param2change] + (
+                        self.foods[i][self.param2change] - self.foods[self.neighbour][self.param2change]) * (
                                                              r - 0.5) * 2
 
-            if _self.solution[_self.param2change] < _self.conf.LOWER_BOUND:
-                _self.solution[_self.param2change] = _self.conf.LOWER_BOUND
-            if _self.solution[_self.param2change] > _self.conf.UPPER_BOUND:
-                _self.solution[_self.param2change] = _self.conf.UPPER_BOUND
-            _self.ObjValSol = _self.calculate_function(_self.solution)[0]
-            _self.FitnessSol = _self.calculate_fitness(_self.ObjValSol)
-            if (_self.FitnessSol > _self.fitness[i] and _self.conf.MINIMIZE == True) or (_self.FitnessSol <= _self.fitness[i] and _self.conf.MINIMIZE == False):
-                _self.trial[i] = 0
-                _self.foods[i][:] = np.copy(_self.solution)
-                _self.f[i] = _self.ObjValSol
-                _self.fitness[i] = _self.FitnessSol
+            if self.solution[self.param2change] < self.conf.LOWER_BOUND:
+                self.solution[self.param2change] = self.conf.LOWER_BOUND
+            if self.solution[self.param2change] > self.conf.UPPER_BOUND:
+                self.solution[self.param2change] = self.conf.UPPER_BOUND
+            self.ObjValSol = self.calculate_function(self.solution)[0]
+            self.FitnessSol = self.calculate_fitness(self.ObjValSol)
+            if (self.FitnessSol > self.fitness[i] and self.conf.MINIMIZE == True) or (self.FitnessSol <= self.fitness[i] and self.conf.MINIMIZE == False):
+                self.trial[i] = 0
+                self.foods[i][:] = np.copy(self.solution)
+                self.f[i] = self.ObjValSol
+                self.fitness[i] = self.FitnessSol
             else:
-                _self.trial[i] = _self.trial[i] + 1
+                self.trial[i] = self.trial[i] + 1
             i += 1
 
-    def calculate_probabilities(_self):
-        maxfit = np.copy(max(_self.fitness))
-        for i in range(_self.conf.FOOD_NUMBER):
-            _self.prob[i] = (0.9 * (_self.fitness[i] / maxfit)) + 0.1
+    def calculate_probabilities(self):
+        maxfit = np.copy(max(self.fitness))
+        for i in range(self.conf.FOOD_NUMBER):
+            self.prob[i] = (0.9 * (self.fitness[i] / maxfit)) + 0.1
 
-    def send_onlooker_bees(_self):
+    def send_onlooker_bees(self):
         i = 0
         t = 0
-        while (t < _self.conf.FOOD_NUMBER) and (not (_self.stopping_condition())):
+        while (t < self.conf.FOOD_NUMBER) and (not (self.stopping_condition())):
             r = random.random()
-            if ((r < _self.prob[i] and _self.conf.MINIMIZE == True) or (r > _self.prob[i] and _self.conf.MINIMIZE == False)):
+            if ((r < self.prob[i] and self.conf.MINIMIZE == True) or (r > self.prob[i] and self.conf.MINIMIZE == False)):
                 t+=1
                 r = random.random()
-                _self.param2change = (int)(r * _self.conf.DIMENSION)
+                self.param2change = (int)(r * self.conf.DIMENSION)
                 r = random.random()
-                _self.neighbour = (int)(r * _self.conf.FOOD_NUMBER)
-                while _self.neighbour == i:
+                self.neighbour = (int)(r * self.conf.FOOD_NUMBER)
+                while self.neighbour == i:
                     r = random.random()
-                    _self.neighbour = (int)(r * _self.conf.FOOD_NUMBER)
-                _self.solution = np.copy(_self.foods[i][:])
+                    self.neighbour = (int)(r * self.conf.FOOD_NUMBER)
+                self.solution = np.copy(self.foods[i][:])
 
                 r = random.random()
-                _self.solution[_self.param2change] = _self.foods[i][_self.param2change] + (
-                            _self.foods[i][_self.param2change] - _self.foods[_self.neighbour][_self.param2change]) * (
+                self.solution[self.param2change] = self.foods[i][self.param2change] + (
+                            self.foods[i][self.param2change] - self.foods[self.neighbour][self.param2change]) * (
                                                                  r - 0.5) * 2
-                if _self.solution[_self.param2change] < _self.conf.LOWER_BOUND:
-                    _self.solution[_self.param2change] = _self.conf.LOWER_BOUND
-                if _self.solution[_self.param2change] > _self.conf.UPPER_BOUND:
-                    _self.solution[_self.param2change] = _self.conf.UPPER_BOUND
+                if self.solution[self.param2change] < self.conf.LOWER_BOUND:
+                    self.solution[self.param2change] = self.conf.LOWER_BOUND
+                if self.solution[self.param2change] > self.conf.UPPER_BOUND:
+                    self.solution[self.param2change] = self.conf.UPPER_BOUND
 
-                _self.ObjValSol = _self.calculate_function(_self.solution)[0]
-                _self.FitnessSol = _self.calculate_fitness(_self.ObjValSol)
-                if (_self.FitnessSol > _self.fitness[i] and _self.conf.MINIMIZE == True) or (_self.FitnessSol <= _self.fitness[i] and _self.conf.MINIMIZE == False):
-                    _self.trial[i] = 0
-                    _self.foods[i][:] = np.copy(_self.solution)
-                    _self.f[i] = _self.ObjValSol
-                    _self.fitness[i] = _self.FitnessSol
+                self.ObjValSol = self.calculate_function(self.solution)[0]
+                self.FitnessSol = self.calculate_fitness(self.ObjValSol)
+                if (self.FitnessSol > self.fitness[i] and self.conf.MINIMIZE == True) or (self.FitnessSol <= self.fitness[i] and self.conf.MINIMIZE == False):
+                    self.trial[i] = 0
+                    self.foods[i][:] = np.copy(self.solution)
+                    self.f[i] = self.ObjValSol
+                    self.fitness[i] = self.FitnessSol
                 else:
-                    _self.trial[i] = _self.trial[i] + 1
+                    self.trial[i] = self.trial[i] + 1
             i += 1
-            i = i % _self.conf.FOOD_NUMBER
+            i = i % self.conf.FOOD_NUMBER
 
-    def send_scout_bees(_self):
-        if np.amax(_self.trial) >= _self.conf.LIMIT:
-            _self.init(_self.trial.argmax(axis = 0))
+    def send_scout_bees(self):
+        if np.amax(self.trial) >= self.conf.LIMIT:
+            self.init(self.trial.argmax(axis = 0))
 
-    def increase_cycle(_self):
-        _self.globalOpts.append(_self.globalOpt)
-        _self.cycle += 1
-    def setExperimentID(_self,run,t):
-        _self.experimentID = t+"-"+str(run)
+    def increase_cycle(self):
+        self.globalOpts.append(self.globalOpt)
+        self.cycle += 1
+    def setExperimentID(self,run,t):
+        self.experimentID = t+"-"+str(run)
--- Reporter.py
+++ Reporter.py
@@ -4,92 +4,92 @@ import numpy as np
 from decimal import Decimal
 
 class Reporter:
-    def __init__(_self, abcList):
-        _self.abcList = abcList
+    def __init__(self, abcList):
+        self.abcList = abcList
         if(abcList[0].conf.PRINT_PARAMETERS):
-            _self.print_parameters()
+            self.print_parameters()
 
         if (abcList[0].conf.RUN_INFO):
-            _self.run_info()
+            self.run_info()
         if (abcList[0].conf.SAVE_RESULTS):
-             _self.save_results()
+             self.save_results()
         if (abcList[0].conf.RUN_INFO_COMMANDLINE):
-            _self.command_line_print()
+            self.command_line_print()
 
 
-    def print_parameters(_self):
-        for i in range(_self.abcList[0].conf.RUN_TIME):
-            print(_self.abcList[i].experimentID,". run")
-            for j in range(_self.abcList[0].conf.DIMENSION):
-                print("Global Param[", j + 1, "] ", _self.abcList[i].globalParams[j])
+    def print_parameters(self):
+        for i in range(self.abcList[0].conf.RUN_TIME):
+            print(self.abcList[i].experimentID,". run")
+            for j in range(self.abcList[0].conf.DIMENSION):
+                print("Global Param[", j + 1, "] ", self.abcList[i].globalParams[j])
 
-    def run_info(_self):
-        sum = []
-        for i in range(_self.abcList[0].conf.RUN_TIME):
-            print(_self.abcList[i].experimentID + " run: ", _self.abcList[i].globalOpt, " Cycle: ",
-                  _self.abcList[i].cycle, " Time: ",
-                  _self.abcList[i].globalTime)
-            sum.append(_self.abcList[i].globalOpt)
-        print("Mean: ",np.mean(sum)," Std: ",np.std(sum)," Median: ",np.median(sum))
-    def command_line_print(_self):
-        sum = []
-        for i in range(_self.abcList[0].conf.RUN_TIME):
-            sum.append(_self.abcList[i].globalOpt)
-        print('%1.5E' % Decimal(np.mean(sum)))
+    def run_info(self):
+        summ = []
+        for i in range(self.abcList[0].conf.RUN_TIME):
+            print(self.abcList[i].experimentID + " run: ", self.abcList[i].globalOpt, " Cycle: ",
+                  self.abcList[i].cycle, " Time: ",
+                  self.abcList[i].globalTime)
+            summ.append(self.abcList[i].globalOpt)
+        print("Mean: ", np.mean(summ), " Std: ", np.std(summ), " Median: ", np.median(summ))
+    def command_line_print(self):
+        summ = []
+        for i in range(self.abcList[0].conf.RUN_TIME):
+            summ.append(self.abcList[i].globalOpt)
+        print('%1.5E' % Decimal(np.mean(summ)))
 
-    def save_results(_self):
-        if not os.path.exists(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME):
-            os.makedirs(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME)
+    def save_results(self):
+        if not os.path.exists(self.abcList[0].conf.OUTPUTS_FOLDER_NAME):
+            os.makedirs(self.abcList[0].conf.OUTPUTS_FOLDER_NAME)
 
         header="experimentID ;Number of Population; Maximum Evaluation; Limit; Function, Dimension, Upper Bound; Lower Bound; isMinimize; Result ; Time \n"
         csvText = "{} ;{}; {}; {}; {}; {}; {}; {}; {}; {} ; {} \n"
-        with open(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME+"/"+_self.abcList[0].conf.RESULT_REPORT_FILE_NAME, 'a') as saveRes:
-            if(sum(1 for line in open(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME+"/"+_self.abcList[0].conf.RESULT_REPORT_FILE_NAME))<1):
+        with open(self.abcList[0].conf.OUTPUTS_FOLDER_NAME+"/"+self.abcList[0].conf.RESULT_REPORT_FILE_NAME, 'a') as saveRes:
+            if(sum(1 for _ in open(self.abcList[0].conf.OUTPUTS_FOLDER_NAME + "/" + self.abcList[0].conf.RESULT_REPORT_FILE_NAME)) < 1):
                 saveRes.write(header)
 
-            for i in range(_self.abcList[0].conf.RUN_TIME):
+            for i in range(self.abcList[0].conf.RUN_TIME):
                 saveRes.write(csvText.format(
-                    _self.abcList[i].experimentID,
-                    _self.abcList[i].conf.NUMBER_OF_POPULATION,
-                    _self.abcList[i].conf.MAXIMUM_EVALUATION,
-                    _self.abcList[i].conf.LIMIT,
-                    _self.abcList[i].conf.OBJECTIVE_FUNCTION.__name__,
-                    _self.abcList[i].conf.DIMENSION,
-                    _self.abcList[i].conf.UPPER_BOUND,
-                    _self.abcList[i].conf.LOWER_BOUND,
-                    _self.abcList[i].conf.MINIMIZE,
-                    _self.abcList[i].globalOpt,
-                    _self.abcList[i].globalTime,
+                    self.abcList[i].experimentID,
+                    self.abcList[i].conf.NUMBER_OF_POPULATION,
+                    self.abcList[i].conf.MAXIMUM_EVALUATION,
+                    self.abcList[i].conf.LIMIT,
+                    self.abcList[i].conf.OBJECTIVE_FUNCTION.__name__,
+                    self.abcList[i].conf.DIMENSION,
+                    self.abcList[i].conf.UPPER_BOUND,
+                    self.abcList[i].conf.LOWER_BOUND,
+                    self.abcList[i].conf.MINIMIZE,
+                    self.abcList[i].globalOpt,
+                    self.abcList[i].globalTime,
 
                 ))
 
             header = "experimentID;"
-            for j in range(_self.abcList[0].conf.DIMENSION):
+            for j in range(self.abcList[0].conf.DIMENSION):
 
-                if (j < _self.abcList[0].conf.DIMENSION - 1):
+                if (j < self.abcList[0].conf.DIMENSION - 1):
                     header = header + "param" + str(j) + ";"
                 else:
                     header = header + "param" + str(j) + "\n"
-            with open(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME + "/" + _self.abcList[0].conf.PARAMETER_REPORT_FILE_NAME,
+            with open(self.abcList[0].conf.OUTPUTS_FOLDER_NAME + "/" + self.abcList[0].conf.PARAMETER_REPORT_FILE_NAME,
                       'a') as saveRes:
-                if (sum(1 for line in open(_self.abcList[0].conf.OUTPUTS_FOLDER_NAME + "/" + _self.abcList[
+                if (sum(1 for _ in open(self.abcList[0].conf.OUTPUTS_FOLDER_NAME + "/" + self.abcList[
                     0].conf.PARAMETER_REPORT_FILE_NAME)) < 1):
                     saveRes.write(header)
 
-                for i in range(_self.abcList[0].conf.RUN_TIME):
-                    csvText=str(_self.abcList[i].experimentID)+";"
-                    for j in range(_self.abcList[0].conf.DIMENSION):
-                        if(j<_self.abcList[0].conf.DIMENSION-1):
-                            csvText = csvText+str(_self.abcList[i].globalParams[j])+";"
+                for i in range(self.abcList[0].conf.RUN_TIME):
+                    csvText=str(self.abcList[i].experimentID)+";"
+                    for j in range(self.abcList[0].conf.DIMENSION):
+                        if(j<self.abcList[0].conf.DIMENSION-1):
+                            csvText = csvText+str(self.abcList[i].globalParams[j])+";"
                         else:
-                            csvText = csvText + str(_self.abcList[i].globalParams[j]) + "\n"
+                            csvText = csvText + str(self.abcList[i].globalParams[j]) + "\n"
                     saveRes.write(csvText)
 
-            for i in range(_self.abcList[0].conf.RUN_TIME):
-                if not os.path.exists(_self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+_self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER):
-                    os.makedirs(_self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+_self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER)
-                with open(_self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+_self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER+"/"+_self.abcList[i].experimentID+".txt",
+            for i in range(self.abcList[0].conf.RUN_TIME):
+                if not os.path.exists(self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER):
+                    os.makedirs(self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER)
+                with open(self.abcList[i].conf.OUTPUTS_FOLDER_NAME+"/"+self.abcList[i].conf.RESULT_BY_CYCLE_FOLDER+"/"+self.abcList[i].experimentID+".txt",
                           'a') as saveRes:
 
-                    for j in range(_self.abcList[i].cycle):
-                        saveRes.write(str(_self.abcList[i].globalOpts[j])+"\n")
+                    for j in range(self.abcList[i].cycle):
+                        saveRes.write(str(self.abcList[i].globalOpts[j])+"\n")
