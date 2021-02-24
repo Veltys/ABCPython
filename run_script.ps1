7b902adb032cbdee89e37cdc7425e5d0d0d49934
Veltys <veltys@veltys.es>
2021-02-24 20:02:12 +0100

    FIX: Damn Windows Unicode BOM

--- run_script.ps1
+++ run_script.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 
 for($i = 1; $i -le 10; $i++) {
-	Write-Output $i > func_num.txt
+	Out-File -FilePath .\func_num.txt -InputObject $i -Encoding ASCII
 
 	for($j = 10; $j -le 20; $j += 5) {
 		Write-Output "Ejecución $i, dimensión $j"
