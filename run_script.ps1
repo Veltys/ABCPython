d2535cc5f052eb1bcf4b69706b5c7ecfe63052e9
Veltys <veltys@veltys.es>
2021-02-25 18:10:15 +0100

    ADD: Execution with parameters

--- run_script.ps1
+++ run_script.ps1
@@ -1,10 +1,31 @@
 #!/usr/bin/env pwsh
 
-for($i = 1; $i -le 10; $i++) {
+if(
+	($args[0] -eq "" -or !($args[0] -match '^\d+$') -or $args[0] -lt 1) -or
+	($args[1] -eq "" -or !($args[1] -match '^\d+$') -or $args[1] -lt 1)
+) {
+	$funciones		= @( 1, 10, 1)
+	$dimensiones	= @(10, 20, 5)
+}
+else {
+	$funciones		= @($args[0], $args[1], 1)
+
+	if(
+		($args[2] -eq "" -or !($args[1] -match '^\d+$') -or $args[2] -lt 10) -or
+		($args[3] -eq "" -or !($args[3] -match '^\d+$') -or $args[3] -lt 10)
+	) {
+		$dimensiones	= @(10, 20, 5)
+	}
+	else {
+		$dimensiones	= @($args[2], $args[3], 5)
+	}
+}
+
+for($i = $funciones[0]; $i -le $funciones[1]; $i += $funciones[2]) {
 	Out-File -FilePath .\func_num.txt -InputObject $i -Encoding ASCII
 
-	for($j = 10; $j -le 20; $j += 5) {
-		Write-Output "Ejecución $i, dimensión $j"
+	for($j = $dimensiones[0]; $j -le $dimensiones[1]; $j += $dimensiones[2]) {
+		Write-Output "Función $i, dimensión $j"
 
 		.\ABCAlgorithm.py -d $j
 	}
--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-for (( i=1; i<=10; i++ )); do
+if [ $# -lt 2 ] || \
+   [[ "$1" =~ '^[0-9]+$' ]] || [ "$1" -lt 1 ] || \
+   [[ "$2" =~ '^[0-9]+$' ]] || [ "$2" -lt 1 ]
+then
+	funciones=(1 10 1)
+	dimensiones=(10 20 5)
+else
+	funciones=($1 $2 1)
+
+	if [ $# -lt 4 ] || \
+	   [[ "$3" =~ '^[0-9]+$' ]] || [ "$3" -lt 10 ] || \
+	   [[ "$4" =~ '^[0-9]+$' ]] || [ "$4" -lt 10 ]
+	then
+		dimensiones=(10 20 5)
+	else
+		dimensiones=($3 $4 5)
+	fi
+fi
+
+for (( i=${funciones[0]}; i<=${funciones[1]}; i++ )); do
 	echo $i > func_num.txt
 
-	for j in {10..20..5}; do
-		echo "Ejecución $i, dimensión $j"
+	for (( j=${dimensiones[0]}; j<=${dimensiones[1]}; j=$j+5 )); do
+		echo "Función $i, dimensión $j"
 
 		./ABCAlgorithm.py -d $j
 	done
