94cde45435e78f96852fe2e53da2d682cf6c55ac
Veltys <veltys@veltys.es>
2021-03-25 22:33:25 +0100

    FIX: Code quality

--- run_script.sh
+++ run_script.sh
@@ -24,7 +24,7 @@ fi
 if \
 	! ( \
 		[ "$1" = '-r' ] || \
-		( [ $# -eq 5 ] && [ $5 = '-r' ] )
+		( [ $# -eq 5 ] && [ "$5" = '-r' ] )
 	)
 then
 # Procesamiento: ejecución del programa
@@ -42,12 +42,12 @@ fi
 archivos=($(ls ./Outputs/ResultByCycle/ | grep txt))
 
 # Iteración de todas las dimensiones
-for (( i=${dimensiones[0]}; i<=${dimensiones[1]}; i+=${dimensiones[2]} )); do
+for (( i=dimensiones[0]; i<=dimensiones[1]; i+=dimensiones[2] )); do
 	# Preparación de la matriz de resultados
 	declare -A res
 
 	# Desplazamiento dimensional
-	offset=$((($i - ${dimensiones[0]}) / ${dimensiones[2]}))
+	offset=$(((i - dimensiones[0]) / dimensiones[2]))
 
 	for (( j=0; j<30; j++ )); do
 		# Coordenada de insercción en la matriz de resultados calculada debido al previsible mal ordenamiento de los archivos
@@ -63,14 +63,14 @@ for (( i=${dimensiones[0]}; i<=${dimensiones[1]}; i+=${dimensiones[2]} )); do
 			# Número de línea a leer
 			numLinea=$(awk "BEGIN {print int($i ^ ($k / 5 - 3) * 150000)}")
 
-			linea=$(sed "$(($numLinea - 1))q;d" ./Outputs/ResultByCycle/${archivos[$(($offset + $j))]})
+			linea=$(sed "$((numLinea - 1))q;d" ./Outputs/ResultByCycle/"${archivos[$((offset + j))]}")
 
 			# Algunas líneas podrían no existir, debido a los criterios de parada
 			if ! [[ -z "$linea" ]]; then
 				res[$k,$l]=$linea
 			else
 				# En tal caso, se copia el resultado de la línea anterior
-				res[$k,$l]=${res[$(($k - 1)),$l]}
+				res[$k,$l]=${res[$((k - 1)),$l]}
 			fi
 
 			# Limpieza de caracteres no imprimibles no deseados que aparecen "because yes"
@@ -86,7 +86,7 @@ for (( i=${dimensiones[0]}; i<=${dimensiones[1]}; i+=${dimensiones[2]} )); do
 		for (( k=0; k<30; k++ )); do
 			echo -n "${res[$j,$k]}" >> "t1_d$i.csv"
 
-			if [ $k -ne 29 ]; then
+			if [ "$k" -ne 29 ]; then
 				echo -n ',' >> "t1_d$i.csv"
 			fi
 		done
