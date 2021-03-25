c417a712a56f1789a7dcdb38565fb7bf1f1b1160
Veltys <veltys@veltys.es>
2021-03-25 22:26:06 +0100

    FIX: Some optimizations

--- run_script.ps1
+++ run_script.ps1
@@ -2,8 +2,9 @@
 
 # Preprocesamiento: variables
 if(
-	($args[0] -eq "" -or !($args[0] -match '^\d+$') -or $args[0] -lt 1) -or
-	($args[1] -eq "" -or !($args[1] -match '^\d+$') -or $args[1] -lt 1)
+	$args.Length -lt 2 -or
+	(!($args[0] -match '^\d+$') -or $args[0] -lt 1) -or
+	(!($args[1] -match '^\d+$') -or $args[1] -lt 1)
 ) {
 	$funciones		= @( 1, 10, 1)
 	$dimensiones	= @(10, 20, 5)
@@ -12,8 +13,9 @@ else {
 	$funciones		= @($args[0], $args[1], 1)
 
 	if(
-		($args[2] -eq "" -or !($args[1] -match '^\d+$') -or $args[2] -lt 5) -or
-		($args[3] -eq "" -or !($args[3] -match '^\d+$') -or $args[3] -lt 5)
+		$args.Length -lt 4 -or
+		(!($args[1] -match '^\d+$') -or $args[2] -lt 5) -or
+		(!($args[3] -match '^\d+$') -or $args[3] -lt 5)
 	) {
 		$dimensiones	= @(10, 20, 5)
 	}
@@ -22,16 +24,22 @@ else {
 	}
 }
 
-# Procesamiento: ejecución del programa
+# Procesamiento condicional a la no existencia del parámetro -r
+if(
+	!(
+		$args[0] -eq '-r' -or
+		($args.Length -eq 5 -and $args[4] -eq '-r')
+	)
+) {
+	# Procesamiento: ejecución del programa
+	for($i = $funciones[0]; $i -le $funciones[1]; $i += $funciones[2]) {
+		for($j = $dimensiones[0]; $j -le $dimensiones[1]; $j += $dimensiones[2]) {
+			Write-Output "Función $i, dimensión $j"
 
-for($i = $funciones[0]; $i -le $funciones[1]; $i += $funciones[2]) {
-	for($j = $dimensiones[0]; $j -le $dimensiones[1]; $j += $dimensiones[2]) {
-		Write-Output "Función $i, dimensión $j"
-
-		.\ABCAlgorithm.py -d $j -o external_benchmark_$i
+			.\ABCAlgorithm.py -d $j -o external_benchmark_$i
+		}
 	}
 }
-
 
 # Posprocesamiento: recopilación de resultados
 # Recogida de todos los archivos de salida
@@ -42,7 +50,7 @@ for($i = $dimensiones[0]; $i -le $dimensiones[1]; $i += $dimensiones[2]) {
 	# Preparación de la matriz de resultados
 	[System.Collections.ArrayList]$res = @()
 
-	for($j = 0; $j -le 15; $j++) {
+	for($j = 0; $j -lt 16; $j++) {
 		$null = $res.Add(@(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 	}
 
@@ -50,18 +58,18 @@ for($i = $dimensiones[0]; $i -le $dimensiones[1]; $i += $dimensiones[2]) {
 	$offset = ($i - $dimensiones[0]) / $dimensiones[2]
 
 
-	for($j = 0; $j -le 29; $j++) {
-		for($k = 0; $k -le 15; $k++) {
+	for($j = 0; $j -lt 30; $j++) {
+		# Coordenada de insercción en la matriz de resultados calculada debido al previsible mal ordenamiento de los archivos
+        $l = $archivos[$j].Name.Split('.')[0].Split('-')[-1]
+
+		for($k = 0; $k -lt 16; $k++) {
 			# Número de línea a leer
 			$numLinea = [Math]::Round([Math]::Pow($i, $k / 5 - 3) * 150000)
-
-			# Coordenada de insercción en la matriz de resultados calculada debido al previsible mal ordenamiento de los archivos
-            $l = $archivos[$j].Name.Split('.')[0].Split('-')[-1]
 
             $linea = Get-Content $archivos[$offset + $j].FullName | Select -Index ($numLinea - 1)
 
 			# Algunas líneas podrían no existir, debido a los criterios de parada
-            if($linea -match '^[-]?[0-9.]+$') {
+            if($linea -match '^[-]?[0-9]+\.?[0-9]*$') {
 				$res[$k][$l] = $linea
 			}
 			else {
@@ -73,12 +81,12 @@ for($i = $dimensiones[0]; $i -le $dimensiones[1]; $i += $dimensiones[2]) {
 
 	$out = '';
 
-	for($j = 0; $j -le 15; $j++) {
-		for($k = 0; $k -le 29; $k++) {
+	for($j = 0; $j -lt 16; $j++) {
+		for($k = 0; $k -lt 30; $k++) {
 			$out += $res[$j][$k]
 
 			 if($k -ne 29) {
-			 	$out += ';'
+			 	$out += ','
 		 	}
 		}
 
